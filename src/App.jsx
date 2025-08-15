7e023c1fb80ba2c1e469845d6a2c762e42df6ede
Lucas Pouey <lucaspouey@gmail.com>
2025-08-15 16:30:09 -0300

    Pokedex
    Fiz alguns cards aimados para a atividade 10.

--- src/App.jsx
+++ src/App.jsx
@@ -19,19 +19,44 @@ export default function App() {
       }}
     >
       <PokemonCard
-        name="Pikachu"
-        img="https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png"
-        type="Elétrico"
+        name="Greninja"
+        img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfaDCRgg6PLKLmBXQG1gSIX54jiJ6DyPKC8Q&s"
+        type="Agua"
       />
       <PokemonCard
-        name="Charmander"
-        img="https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png"
-        type="Fogo"
+        name="Rillabom"
+        img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSAnotWlXE3RtMDOlQdINsh_aS2DTaim4IWw&s"
+        type="Grama"
       />
       <PokemonCard
-        name="Squirtle"
-        img="https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png"
-        type="Água"
+        name="Garchomp"
+        img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbIIFfP3sd9jT3_QiTxea5C1QHc_-pQNN39Q&s"
+        type="Dragao"
+      />
+      <PokemonCard
+        name="Sceptile"
+        img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpyj0DZ-1eopPzY-HSn7y5RyXLvhwqoEUKag&s"
+        type="Grama"
+      />
+      <PokemonCard
+        name="Luxray"
+        img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9N3Xkhj_8KtDmHKYcDzMK4V5Isjm7yxCLpQ&s"
+        type="Eletrico"
+      />
+      <PokemonCard
+        name="Glaceon"
+        img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKHBCSKAu3edHqHOdIViR3qImor0ejp0paIg&s"
+        type="Gelo"
+      />
+      <PokemonCard
+        name="Gengar"
+        img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2WiX4-RkatB58A0RpGOAbanyWwSaGuEd-9g&s"
+        type="Sombrio"
+      />
+      <PokemonCard
+        name="Heracross"
+        img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzS9wM5-7Z7G9TFmiEk1cSCIHeXNFxzc-wpA&s"
+        type="Lutador"
       />
     </div>
   );
